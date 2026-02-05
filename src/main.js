845c4a3f8277c454950af3bd6ced482936e7ebed
Jason Koss <jasonkoss@Jasons-MacBook-Air.local>
2026-02-05 00:20:15 -0800

    Bump map bundle version

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { fetchVenues, updateAllVenueStatuses } from './data.js?v=13';
-import { initMap, getMap, renderMarkers, fitToVenues } from './map.v13.js?v=55';
+import { initMap, getMap, renderMarkers, fitToVenues } from './map.v13.js?v=56';
 
 // --- Configuration ---
 // Replace with your published Google Sheet CSV URL
